d582821096dccb63daaf670df45f80cef4c754a1
LopesManuel <lpsmanuel@gmail.com>
2016-07-19 17:51:45 +0100

    update

--- run2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-for i in {1..10000}
-do
-    echo "---------------------------------"
-    ./main  -l Lvls/lvl0.txt -n 4 -p player1 player1 plannerK2 plannerK2 -seed $i -f 
-    echo "---------------------------------"
-done
